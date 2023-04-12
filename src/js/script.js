59c73938c6874c46a8fefabcdb58d5109195a18b
Kate2111 <noskova2111@mail.ru>
2023-04-12 14:24:38 +0400

    media video block

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const videoButton = document.querySelector('.video__button');
+
+    showVideo(videoButton);
+
+    function showVideo(button) {
+        button.addEventListener('click', playVideo);
+    }
+
+    function playVideo(event) {
+        event.preventDefault();
+
+        const videoLink = `<iframe class="video__content" src=${this.dataset.video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+
+        this.insertAdjacentHTML('afterend', videoLink);
+        this.removeEventListener('click', playVideo);
+    }
+
+
+})
